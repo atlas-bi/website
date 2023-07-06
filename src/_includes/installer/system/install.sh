c018978405d6ec5b02ded1f35ab0c26328e09947
Christopher Pickering <christopher@going.bg>
2023-07-06 15:05:14 -0500

    removed unused code

--- src/_includes/installer/system/install.sh
+++ src/_includes/installer/system/install.sh
@@ -16,11 +16,9 @@ fmt_yellow "Finding a free port.."
 
 PORT=$(get_port)
 QUIRREL_PORT=$(get_port)
-MEILI_PORT=$(get_port)
 
 fmt_blue "Using web port $PORT"
 fmt_blue "Using quirrel port $QUIRREL_PORT"
-fmt_blue "Using meilsearch port $MEILI_PORT"
 
 # Download the latest release.
 fmt_yellow "Downloading latest version into $(pwd)/$PORT.."
@@ -46,20 +44,13 @@ npx prisma migrate deploy
 # Set a few process names.
 APP_PROCESS="$APP-$PORT"
 QUIRREL_PROCESS="$APP-quirrel-$QUIRREL_PORT"
-MEILI_PROCESS="$APP-meili-$MEILI_PORT"
 
 exporter NODE_ENV=production
 exporter WEB_PORT=$PORT
 exporter QUIRREL_PORT=$QUIRREL_PORT
-exporter MEILI_PORT=$MEILI_PORT
 
 exporter APP_PROCESS=$APP_PROCESS
 exporter QUIRREL_PROCESS=$QUIRREL_PROCESS
-exporter MEILI_PROCESS=$MEILI_PROCESS
-
-# Download meilisearch.
-fmt_yellow "Installing meilisearch.."
-curl -L https://install.meilisearch.com | sh
 
 fmt_yellow "Starting new services.."
 
@@ -77,19 +68,8 @@ exporter QUIRREL_API_URL=http://localhost:$QUIRREL_PORT
 # Load quirrel cron jobs.
 npm run quirrel:ci
 
-# Add a few env vars to get meilisearch running
-exporter MEILI_NO_ANALYTICS=true
-exporter MEILI_DB_PAT=$(pwd)/$PORT/data.ms/
-exporter MEILI_ENV=production
-exporter MEILI_MASTER_KEY=$MEILI_PROCESS
-exporter MEILISEARCH_URL=http://localhost:$MEILI_PORT
-exporter MEILI_HTTP_ADDR=localhost:$MEILI_PORT
-
 # Start web process.
 dotenv -v PORT=$PORT -- pm2 start node --name="$APP_PROCESS" --merge-logs -- ./build/server.js
-
-# Start meili process.
-dotenv -- pm2 start meilisearch --name="$MEILI_PROCESS"
 
 fmt_blue "Done setting up."
 cd ..
@@ -140,7 +120,6 @@ ${YELLOW}$(cat $PORT/.env.local)
 
 ${YELLOW}Web process was started with ${BLUE}dotenv -v PORT=$PORT -- pm2 start node -i -1 --name="$APP_PROCESS" --merge-logs -- ./build/server.js
 ${YELLOW}Quirrel process was started with ${BLUE}dotenv -v PORT=$QUIRREL_PORT -- pm2 start node --name="$QUIRREL_PROCESS" -- node_modules/quirrel/dist/cjs/src/api/main.js
-${YELLOW}Meilisearch process was started with ${BLUE}dotenv -- pm2 start meilisearch --name="$MEILI_PROCESS"
 
 ${CYAN}Updating App Settings
 
@@ -148,7 +127,6 @@ ${YELLOW}1. Update user configuration file ${BLUE}nano $(pwd)/.env
 ${YELLOW}2. Copy config into app ${BLUE}cp $(pwd)/.env $(pwd)/$PORT/.env
 ${YELLOW}3. Restart the apps:
 ${BLUE}   pm2 restart $APP_PROCESS
-${BLUE}   pm2 restart $MEILI_PROCESS
 ${BLUE}   pm2 restart $QUIRREL_PROCESS
 
 ${CYAN}Updating Nginx Settings
